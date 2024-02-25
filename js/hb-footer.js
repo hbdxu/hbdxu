f5e3c56f35387cc1b7eb83eaf10ca218a861790b
hbdxu <48580000+hbdxu@users.noreply.github.com>
2024-02-25 18:36:36 +0800

    Add files via upload

---
+++ js/hb-footer.js
@@ -0,0 +1,31 @@
+let img01 = document.querySelectorAll("#hb-bottom img")[0];
+let img01Onclick = document.createAttribute("onclick");
+img01Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img01.attributes.setNamedItem(img01Onclick);
+
+let img02 = document.querySelectorAll("#hb-bottom img")[1];
+let img02Onclick = document.createAttribute("onclick");
+img02Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img02.attributes.setNamedItem(img02Onclick);
+
+let img03 = document.querySelectorAll("#hb-bottom img")[2];
+let img03Onclick = document.createAttribute("onclick");
+img03Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img03.attributes.setNamedItem(img03Onclick);
+
+let img04 = document.querySelectorAll("#hb-bottom img")[3];
+let img04Onclick = document.createAttribute("onclick");
+img04Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img04.attributes.setNamedItem(img04Onclick);
+
+let img05 = document.querySelectorAll("#hb-bottom img")[4];
+let img05Onclick = document.createAttribute("onclick");
+img05Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img05.attributes.setNamedItem(img05Onclick);
+
+let img06 = document.querySelectorAll("#hb-bottom img")[5];
+let img06Onclick = document.createAttribute("onclick");
+img06Onclick.nodeValue = "location.href='./html/hb-404.html'";
+img06.attributes.setNamedItem(img06Onclick);
+
+
